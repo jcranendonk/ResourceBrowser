ac3ed66c5aa0feb6a84194117a4896a7b674ba27
jcranendonk <jeroen.cranendonk@gmail.com>
2013-06-02 22:05:54 -0600

    Initial commit.

---
+++ ResourceBrowser/ResourceBrowser.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Resources;
+using System.Collections;
+
+namespace ResourceBrowser
+{
+    public partial class ResourceBrowser : Form
+    {
+        public ResourceBrowser()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click( object sender, EventArgs e )
+        {
+            if( ofdAssembly.ShowDialog() == DialogResult.OK )
+            {
+                try
+                {
+                    ShowInfo( Assembly.LoadFile( ofdAssembly.FileName ) );
+                }
+                catch
+                {
+                    ShowInfo( null );
+                    lblStatus.Text = "Failed to load assembly.";
+                }
+            }
+        }
+
+        private void ShowInfo( Assembly asm )
+        {
+            tvResources.Nodes.Clear();
+            pbValue.Visible = false;
+            tbValue.Visible = false;
+            btnExport.Enabled = false;
+
+            if( asm != null )
+            {
+                try
+                {
+                    lblStatus.Text = "Loaded: " + asm.FullName;
+
+                    foreach( string resourceName in asm.GetManifestResourceNames() )
+                    {
+                        TreeNode root = new TreeNode( resourceName );
+                        root.ImageIndex = 1;
+                        root.SelectedImageIndex = 1;
+                        tvResources.Nodes.Add( root );
+
+                        using( Stream resourceStream = asm.GetManifestResourceStream( resourceName ) )
+                        {
+                            try
+                            {
+                                IDictionaryEnumerator enu = new ResourceSet( resourceStream ).GetEnumerator();
+
+                                while( enu.MoveNext() )
+                                {
+                                    TreeNode node = new TreeNode( enu.Key.ToString() );
+                                    root.Nodes.Add( node );
+                                    node.ImageIndex = 0;
+                                    node.SelectedImageIndex = 0;
+
+                                    if( enu.Value is Icon || enu.Value is Bitmap )
+                                    {
+                                        node.Tag = enu.Value;
+                                        node.ImageIndex = 2;
+                                        node.SelectedImageIndex = 2;
+                                    }
+                                    else if( enu.Value is String )
+                                    {
+                                        node.Tag = enu.Value;
+                                        node.ImageIndex = 3;
+                                        node.SelectedImageIndex = 3;
+                                    }
+                                    else if( enu.Value is ImageListStreamer )
+                                    {
+                                        ImageList il = new ImageList();
+                                        il.ImageStream = (ImageListStreamer)enu.Value;
+                                        node.ImageIndex = 1;
+                                        node.SelectedImageIndex = 1;
+
+                                        for( int i = 0; i < il.Images.Count; i++ )
+                                        {
+                                            TreeNode imgNode = new TreeNode( i.ToString() );
+                                            imgNode.Tag = il.Images[i];
+                                            imgNode.ImageIndex = 2;
+                                            imgNode.SelectedImageIndex = 2;
+
+                                            node.Nodes.Add( imgNode );
+                                        }
+                                    }
+                                }
+                            }
+                            catch
+                            {
+                                try
+                                {
+                                    //------------------------------------------------------
+                                    //  Try treating stream as a single image.
+                                    //------------------------------------------------------
+                                    root.Tag = new Bitmap( resourceStream );
+                                    root.ImageIndex = 2;
+                                    root.SelectedImageIndex = 2;
+                                }
+                                catch
+                                {
+                                    try
+                                    {
+                                        //------------------------------------------------------
+                                        //  Try treating stream as plain text.
+                                        //------------------------------------------------------
+                                        using( TextReader tr = new StreamReader( resourceStream ) )
+                                        {
+                                            root.Tag = tr.ReadToEnd();
+                                            root.ImageIndex = 0;
+                                            root.SelectedImageIndex = 0;
+                                        }
+                                    }
+                                    catch
+                                    {
+                                        lblStatus.Text = "Error reading resource file.";
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch( Exception ex )
+                {
+                    //------------------------------------------------------
+                    //  Exception likely thrown by GetManifestResourceNames().
+                    //------------------------------------------------------
+                    lblStatus.Text = ex.Message;
+                }
+            }
+            else
+            {
+                lblStatus.Text = "No assembly loaded.";
+            }
+        }
+
+        private void tvResources_AfterSelect( object sender, TreeViewEventArgs e )
+        {
+            ShowOneObject( e.Node.Tag, e.Node.Text );
+        }
+
+        private void ShowOneObject( object obj, string name )
+        {
+            sfdAssembly.FileName = "";
+
+            try
+            {
+                if( obj != null )
+                {
+                    if( obj is Icon )
+                    {
+                        tbValue.Visible = false;
+                        pbValue.Visible = true;
+                        cbFitImage.Enabled = true;
+                        btnExport.Enabled = true;
+
+                        pbValue.Image = ( (Icon)obj ).ToBitmap();
+                        sfdAssembly.Filter = "Icon files|*.ico";
+                        sfdAssembly.FileName = name + ".ico";
+                    }
+                    else if( obj is String )
+                    {
+                        tbValue.Visible = true;
+                        pbValue.Visible = false;
+                        btnExport.Enabled = true;
+
+                        tbValue.Text = obj.ToString();
+                        sfdAssembly.Filter = "Text files|*.txt";
+                        sfdAssembly.FileName = name + ".txt";
+                    }
+                    else if( obj is Image || obj is Bitmap )
+                    {
+                        tbValue.Visible = false;
+                        pbValue.Visible = true;
+                        cbFitImage.Enabled = true;
+                        btnExport.Enabled = true;
+
+                        pbValue.Image = (Image)obj;
+                        sfdAssembly.Filter = "PNG files|*.png|Bitmap files|*.bmp";
+                        sfdAssembly.FileName = name + ".png";
+                    }
+                }
+                else
+                {
+                    tbValue.Visible = false;
+                    pbValue.Visible = false;
+                    cbFitImage.Enabled = false;
+                    btnExport.Enabled = false;
+                }
+            }
+            catch( Exception ex )
+            {
+                tbValue.Visible = true;
+                pbValue.Visible = false;
+                btnExport.Enabled = false;
+
+                tbValue.Text = ex.Message;
+            }
+        }
+
+        private void btnExport_Click( object sender, EventArgs e )
+        {
+            char[] invalidChars = Path.GetInvalidFileNameChars();
+            sfdAssembly.FileName = new string( sfdAssembly.FileName.ToCharArray().Where( c => invalidChars.Contains( c ) == false ).ToArray() );
+
+            if( sfdAssembly.ShowDialog() == DialogResult.OK )
+            {
+                if( pbValue.Visible == true )
+                {
+                    pbValue.Image.Save( sfdAssembly.FileName );
+                }
+                else if( tbValue.Visible == true )
+                {
+                    using( StreamWriter sw = new StreamWriter( sfdAssembly.FileName ) )
+                    {
+                        sw.WriteLine( tbValue.Text );
+                    }
+                }
+            }
+        }
+
+        private void ResourceBrowser_DragDrop( object sender, DragEventArgs e )
+        {
+            if( e.Data.GetDataPresent( DataFormats.FileDrop ) )
+            {
+                string[] files = (string[])e.Data.GetData( DataFormats.FileDrop );
+
+                try
+                {
+                    ShowInfo( Assembly.LoadFrom( files[0] ) );
+                }
+                catch
+                {
+                    ShowInfo( null );
+                }
+            }
+        }
+
+        private void ResourceBrowser_DragEnter( object sender, DragEventArgs e )
+        {
+            if( e.Data.GetDataPresent( DataFormats.FileDrop ) )
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void cbFitImage_CheckedChanged( object sender, EventArgs e )
+        {
+            switch( cbFitImage.CheckState )
+            {
+                case CheckState.Checked:
+                    pbValue.SizeMode = PictureBoxSizeMode.Zoom;
+                    break;
+                case CheckState.Indeterminate:
+                case CheckState.Unchecked:
+                default:
+                    pbValue.SizeMode = PictureBoxSizeMode.CenterImage;
+                    break;
+            }
+        }
+    }
+}
---
+++ ResourceBrowser/ResourceBrowser.designer.cs
@@ -0,0 +1,212 @@
+﻿namespace ResourceBrowser
+{
+    partial class ResourceBrowser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager( typeof( ResourceBrowser ) );
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tvResources = new System.Windows.Forms.TreeView();
+            this.ilResources = new System.Windows.Forms.ImageList( this.components );
+            this.tbValue = new System.Windows.Forms.TextBox();
+            this.pbValue = new System.Windows.Forms.PictureBox();
+            this.ofdAssembly = new System.Windows.Forms.OpenFileDialog();
+            this.sfdAssembly = new System.Windows.Forms.SaveFileDialog();
+            this.cbFitImage = new System.Windows.Forms.CheckBox();
+            ( (System.ComponentModel.ISupportInitialize)( this.splitContainer1 ) ).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ( (System.ComponentModel.ISupportInitialize)( this.pbValue ) ).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point( 12, 12 );
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size( 75, 23 );
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load file";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler( this.btnLoad_Click );
+            // 
+            // btnExport
+            // 
+            this.btnExport.Anchor = ( (System.Windows.Forms.AnchorStyles)( ( System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left ) ) );
+            this.btnExport.Enabled = false;
+            this.btnExport.Location = new System.Drawing.Point( 12, 452 );
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size( 75, 23 );
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "Save value";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler( this.btnExport_Click );
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point( 93, 17 );
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size( 0, 13 );
+            this.lblStatus.TabIndex = 2;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ( (System.Windows.Forms.AnchorStyles)( ( ( ( System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom )
+                        | System.Windows.Forms.AnchorStyles.Left )
+                        | System.Windows.Forms.AnchorStyles.Right ) ) );
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point( 12, 41 );
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add( this.tvResources );
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add( this.tbValue );
+            this.splitContainer1.Panel2.Controls.Add( this.pbValue );
+            this.splitContainer1.Size = new System.Drawing.Size( 709, 405 );
+            this.splitContainer1.SplitterDistance = 264;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // tvResources
+            // 
+            this.tvResources.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvResources.ImageIndex = 0;
+            this.tvResources.ImageList = this.ilResources;
+            this.tvResources.Location = new System.Drawing.Point( 0, 0 );
+            this.tvResources.Name = "tvResources";
+            this.tvResources.SelectedImageIndex = 0;
+            this.tvResources.Size = new System.Drawing.Size( 264, 405 );
+            this.tvResources.TabIndex = 0;
+            this.tvResources.AfterSelect += new System.Windows.Forms.TreeViewEventHandler( this.tvResources_AfterSelect );
+            // 
+            // ilResources
+            // 
+            this.ilResources.ImageStream = ( (System.Windows.Forms.ImageListStreamer)( resources.GetObject( "ilResources.ImageStream" ) ) );
+            this.ilResources.TransparentColor = System.Drawing.Color.Transparent;
+            this.ilResources.Images.SetKeyName( 0, "empty.png" );
+            this.ilResources.Images.SetKeyName( 1, "folder.png" );
+            this.ilResources.Images.SetKeyName( 2, "image.png" );
+            this.ilResources.Images.SetKeyName( 3, "text.png" );
+            // 
+            // tbValue
+            // 
+            this.tbValue.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbValue.Location = new System.Drawing.Point( 0, 0 );
+            this.tbValue.Multiline = true;
+            this.tbValue.Name = "tbValue";
+            this.tbValue.ReadOnly = true;
+            this.tbValue.Size = new System.Drawing.Size( 441, 405 );
+            this.tbValue.TabIndex = 1;
+            this.tbValue.Visible = false;
+            // 
+            // pbValue
+            // 
+            this.pbValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbValue.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbValue.Location = new System.Drawing.Point( 0, 0 );
+            this.pbValue.Name = "pbValue";
+            this.pbValue.Size = new System.Drawing.Size( 441, 405 );
+            this.pbValue.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbValue.TabIndex = 0;
+            this.pbValue.TabStop = false;
+            this.pbValue.Visible = false;
+            // 
+            // ofdAssembly
+            // 
+            this.ofdAssembly.DefaultExt = "dll";
+            this.ofdAssembly.Filter = "Assembly files|*.dll;*.exe";
+            // 
+            // sfdAssembly
+            // 
+            this.sfdAssembly.Filter = "All files|*.*";
+            this.sfdAssembly.FilterIndex = 0;
+            this.sfdAssembly.SupportMultiDottedExtensions = true;
+            // 
+            // cbFitImage
+            // 
+            this.cbFitImage.Anchor = ( (System.Windows.Forms.AnchorStyles)( ( System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right ) ) );
+            this.cbFitImage.AutoSize = true;
+            this.cbFitImage.Enabled = false;
+            this.cbFitImage.Location = new System.Drawing.Point( 653, 16 );
+            this.cbFitImage.Name = "cbFitImage";
+            this.cbFitImage.Size = new System.Drawing.Size( 68, 17 );
+            this.cbFitImage.TabIndex = 4;
+            this.cbFitImage.Text = "Fit image";
+            this.cbFitImage.UseVisualStyleBackColor = true;
+            this.cbFitImage.CheckedChanged += new System.EventHandler( this.cbFitImage_CheckedChanged );
+            // 
+            // ResourceBrowser
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size( 733, 487 );
+            this.Controls.Add( this.cbFitImage );
+            this.Controls.Add( this.splitContainer1 );
+            this.Controls.Add( this.lblStatus );
+            this.Controls.Add( this.btnExport );
+            this.Controls.Add( this.btnLoad );
+            this.Icon = ( (System.Drawing.Icon)( resources.GetObject( "$this.Icon" ) ) );
+            this.Name = "ResourceBrowser";
+            this.Text = "Resource Browser";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler( this.ResourceBrowser_DragDrop );
+            this.DragEnter += new System.Windows.Forms.DragEventHandler( this.ResourceBrowser_DragEnter );
+            this.splitContainer1.Panel1.ResumeLayout( false );
+            this.splitContainer1.Panel2.ResumeLayout( false );
+            this.splitContainer1.Panel2.PerformLayout();
+            ( (System.ComponentModel.ISupportInitialize)( this.splitContainer1 ) ).EndInit();
+            this.splitContainer1.ResumeLayout( false );
+            ( (System.ComponentModel.ISupportInitialize)( this.pbValue ) ).EndInit();
+            this.ResumeLayout( false );
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView tvResources;
+        private System.Windows.Forms.TextBox tbValue;
+        private System.Windows.Forms.PictureBox pbValue;
+        private System.Windows.Forms.OpenFileDialog ofdAssembly;
+        private System.Windows.Forms.SaveFileDialog sfdAssembly;
+        private System.Windows.Forms.ImageList ilResources;
+        private System.Windows.Forms.CheckBox cbFitImage;
+    }
+}
+
